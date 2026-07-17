f3fffba34170ea04ef1ebcfaa0f0b855b9054612
Ghanu00 <ghanshyamtripathi18@gmail.com>
2026-07-17 22:56:10 +0530

    Update base URL to root path (/) in vite.config.ts for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cakesfloor/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
